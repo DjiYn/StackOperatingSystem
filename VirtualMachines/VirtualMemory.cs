206533039f7d04cd3c82272014b3d91606e47874
RasUrb <rasau2000@gmail.com>
2023-03-30 20:40:24 +0300

    Parasyta funkcijos i VirtualMemory

--- VirtualMachines/VirtualMemory.cs
+++ VirtualMachines/VirtualMemory.cs
@@ -8,7 +8,43 @@ namespace StackOperatingSystem.VirtualMachines
 {
     internal class VirtualMemory
     {
-        public VirtualMemory() { }
+        
+        char[] vMemory;
+
+        public VirtualMemory() 
+        { 
+            this.vMemory = new char[Settings.blockSize*Settings.wordSize];
+        }
+        public VirtualMemory(char[] progMemory) 
+        { 
+            this.vMemory = new char[Settings.blockSize*Settings.wordSize];
+            this.vMemory = progMemory;
+        }
+
+        public char readByte(int address)
+        {
+            return this.vMemory[address];
+        }
+
+        public void writeByte(int address, char simbole)
+        {
+            this.vMemory[address]= simbole;
+        }
+
+        public char[] readMemory()
+        {
+            return this.vMemory;
+        }
+
+        // public void writeMemory(char[] text)
+        // {
+        //     if(text.Length < Settings.blockSize*Settings.wordSize)
+        //     {
+        //        this.vMemory = text; 
+        //     }
+        // }
+
+
 
 
     }
