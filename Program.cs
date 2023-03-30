88b79d0df3f080a1247f25268c3971eca9c8dbfe
Orestas <oreatas@gmail.com>
2023-03-30 14:27:07 +0300

    File tree updated, added class virtual machine.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace StackOperatingSystem
+﻿using StackOperatingSystem.Virtual_Machine;
+
+namespace StackOperatingSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            VirtualMachine vm = new VirtualMachine();
         }
     }
 }
